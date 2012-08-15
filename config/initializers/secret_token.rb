c4fdc295cd40b6e234e99d0425c2d66627ea4a4e
Eric Luo <eric.wenbl@gmail.com>
2012-08-15 09:21:23 +0800

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tms::Application.config.secret_token = 'c5f1ccaa794909c8a93230b242192eba36010b07e97a830ec409b7acd864293167475d9fcdb1bcc8f11d4cc2350476850ef35c5276fa4fcb2febbdaaa768704a'
+Tms::Application.config.secret_token = '6fba03d5b1bc71484700aa96cc5cdadb6bf484b57c03693d962e624c07e603d209567afa9d9cd30ebc04884b0eb3088f96b831323d3cbf3670763f686b407108'
