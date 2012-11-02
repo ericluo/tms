7c99ce0d854eeef170d954e50a01cd0f1764cc01
Eric Luo <eric.wenbl@gmail.com>
2012-11-02 12:04:58 +0800

    first runnable report

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,8 +5,22 @@ class Report < ActiveRecord::Base
   attr_accessible :content, :end_date, :start_date, :title
   attr_accessible :category
 
-  def score_rank
+  ScoreRank = Struct.new(:department, :name, :position, :score, :rank)
 
+  def rank_list
+    data = []
+
+    Department.order("id ASC").each do |d|
+      d.users.order("position_id ASC").each do |u|
+        data << ScoreRank.new(d.name, u.name, u.position.name, u.total_score)
+      end
+    end
+
+    scores = data.collect(&:score).sort{|a,b| b <=> a}
+    data.each do |r|
+      r.rank = scores.find_index(r.score) + 1
+    end
+    
   end
-  
+
 end
