e47d03edf6358199bc09685021514549f48aa388
Eric Luo <eric.wenbl@gmail.com>
2012-12-05 14:23:14 +0800

    remove report_definition

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -39,12 +39,6 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit
-    @report = Report.find(params[:id])
-  end
-
-  # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(params[:report])
 
@@ -62,22 +56,6 @@ class ReportsController < ApplicationController
         format.html { redirect_to reports_url, notice: '新增报表成功' }
       else
         format.html { render action: "new" }
-      end
-    end
-  end
-
-  # PUT /reports/1
-  # PUT /reports/1.json
-  def update
-    @report = Report.find(params[:id])
-
-    respond_to do |format|
-      if @report.update_attributes(params[:report])
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/models/report_definition.rb
+++
@@ -1,7 +0,0 @@
-class ReportDefinition < ActiveRecord::Base
-  validates :name, :template, presence: true
-  validates :name, uniqueness: true
-  attr_accessible :name, :template
-
-  has_many :reports
-end
